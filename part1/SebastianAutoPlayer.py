9030341ba820c0f33acafaf91137bdf59c3f15bc
Scheumann, Neelan <nscheuma@iu.edu>
2020-10-24 23:18:50 -0400

    Tweak the choose_score function

--- part1/SebastianAutoPlayer.py
+++ part1/SebastianAutoPlayer.py
@@ -27,13 +27,21 @@ from SebastianState import Scorecard
 import random
 
 class SebastianAutoPlayer:
-    
-    # Code taken from Sebastian.py
+
+
+    def __init__(self):
+        self.numbers = { "primis" : 1, "secundus" : 2, "tertium" : 3, "quartus" : 4, "quintus" : 5, "sextus" : 6 }
+        self.upper_cats = [ "primis", "secundus", "tertium", "quartus", "quintus", "sextus" ]
+        self.lower_cats = [ "triplex", "quadrupla", "squadron", "prattle", "company", "quintuplicatam", "pandemonium" ]
+
+    def combo(self, rolls): 
+        return [(a1, a2, a3, a4, a5) for a1 in rolls[0] for a2 in rolls[1] for a3 in rolls[2] for a4 in rolls[3] \
+                for a5 in rolls[4]]
+
     def roll_score(self, dice, category):
-        numbers = { "primis" : 1, "secundus" : 2, "tertium" : 3, "quartus" : 4, "quintus" : 5, "sextus" : 6 }
         counts = [dice.count(i) for i in range(1,7)]
-        if category in numbers:
-            score = counts[numbers[category]-1] * numbers[category]
+        if category in self.numbers:
+            score = counts[self.numbers[category]-1] * self.numbers[category]
         elif category == "company":
             score = 40 if sorted(dice) == [1,2,3,4,5] or sorted(dice) == [2,3,4,5,6] else 0
         elif category == "prattle":
@@ -48,48 +56,52 @@ class SebastianAutoPlayer:
             score = 50 if max(counts) == 5 else 0
         elif category == "pandemonium":
             score = sum(dice)
-        return score    
-    
+        return score
+
     def choose_score(self, dice, scorecard):
-        score_dict = dict([(category, self.roll_score(dice, category)) for category in self.open_cats])
+        available_upper_cats = [category for category in self.upper_cats if category not in scorecard]
+        available_lower_cats = [category for category in self.lower_cats[:-1] if category not in scorecard]
+        score_dict = dict([(category, self.roll_score(dice, category)) for category in available_upper_cats + available_lower_cats if category not in scorecard])
+        if self.roll_score(dice, 'quintuplicatam')  == 50 and 'quintuplicatam' in available_lower_cats:
+            return 50
+        if self.roll_score(dice, 'squadron')  == 25 and 'squadron' in available_lower_cats:
+            return 25
+        for cat in available_upper_cats:
+            if self.roll_score(dice, cat) >= self.numbers[cat] * 4:
+                return self.roll_score(dice, cat)
+            elif self.roll_score(dice, cat) >= self.numbers[cat] * 3:
+                return self.roll_score(dice, cat)
+        return max(score_dict.items(), key=lambda i: i[1])[1]
+    
+    def choose_category(self, dice, scorecard):
+        available_upper_cats = [category for category in self.upper_cats if category not in scorecard]
+        available_lower_cats = [category for category in self.lower_cats if category not in scorecard]
+        score_dict = dict([(category, self.roll_score(dice, category)) for category in available_upper_cats+available_lower_cats if category not in scorecard])
+        if self.roll_score(dice, 'quintuplicatam')  == 50 and 'quintuplicatam' in available_lower_cats:
+            return 'quintuplicatam'
+        if self.roll_score(dice, 'squadron')  == 25 and 'squadron' in available_lower_cats:
+            return 'squadron'
+        for cat in available_upper_cats:
+            if self.roll_score(dice, cat) >= self.numbers[cat] * 4:
+                return cat
+            elif self.roll_score(dice, cat) >= self.numbers[cat] * 3:
+                return cat
         return max(score_dict.items(), key=lambda i: i[1])[0]
-    
-    # Un-used but would be good to use in expected values
-    def store_open_cats(self, scorecard):
-        self.open_cats = [cats for cats in self.all_cats if cats not in scorecard.scorecard.keys()]  
-        return
-    
-    # Following function is modeled off of combos3() function from 
-    # Module 10.6 in CSSI 551 Fall 2020
-    def combo(self, rolls): 
-        return [(a1, a2, a3, a4, a5) for a1 in rolls[0] for a2 in rolls[1] for a3 in rolls[2] for a4 in rolls[3] \
-                for a5 in rolls[4]]
-    
-    # Following function is modeled off of expectation_of_reroll() function from 
-    # Module 10.6 in CSSI 551 Fall 2020
-    def expected_value(self, roll, reroll):
+
+    def expected_value(self, roll, reroll, scorecard):
         outcomes = self.combo([((roll[die],) if not reroll[die] else range(1, 7)) for die in range(0,5)])
-        return sum([self.roll_score(outcome, cat) for cat in self.open_cats for outcome in outcomes]) / len(outcomes)
-    
-    # Following function is modeled off of max_layer() function from 
-    # Module 10.6 in CSSI 551 Fall 2020
-    def best_strat(self, roll):
-        best_roll = max([(reroll, self.expected_value(roll, reroll)) for reroll in self.combo(((True, False),) * 5)], key = lambda item:item[1])
+        return sum([self.choose_score(outcome, scorecard) for outcome in outcomes]) / len(outcomes)
+        
+    def best_strat(self, roll, scorecard):
+        best_roll = max([(reroll, self.expected_value(roll, reroll, scorecard)) for reroll in self.combo(((True, False),) * 5)], key = lambda item:item[1])
         return [i for i in range(0,5) if best_roll[0][i]]
-    
-    def __init__(self):
-        self.open_cats = []
-        self.all_cats = ["primis", "secundus", "tertium", "quartus", "quintus", "sextus", "company", "prattle", \
-                         "squadron", "triplex", "quadrupla", "quintuplicatam", "pandemonium"]
-        pass  
-    
+
+
     def first_roll(self, dice, scorecard):
-        self.store_open_cats(scorecard)
-        return self.best_strat(dice.dice) # always re-roll first die (blindly)
-    
+        return self.best_strat(dice.dice, scorecard.scorecard)
+
     def second_roll(self, dice, scorecard):
-        self.store_open_cats(scorecard)
-        return self.best_strat(dice.dice) # always re-roll second and third dice (blindly)
+        return self.best_strat(dice.dice, scorecard.scorecard)
     
     def third_roll(self, dice, scorecard):
-        return self.choose_score(dice.dice, scorecard)
+        return self.choose_category(dice.dice, scorecard.scorecard)
