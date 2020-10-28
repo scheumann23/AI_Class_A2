d272b57d118554c50d1b01a3356403b7d06cbfe1
vishal <vibhalla@iu.edu>
2020-10-28 02:22:22 -0700

    Updated with new evaluation functions

--- part2/moveEval.py
+++ part2/moveEval.py
@@ -11,7 +11,103 @@ import numpy as np
 
 points = {'p': 100,'n': 320,'b': 330,'r': 500,'q': 900,'k': 20000, 'P': 100,'N': 320,'B': 330,'R': 500,'Q': 900,'K': 20000}
 
-def getVal(piece):
+wparakeet = [[  0,   0,   0,   0,   0,   0,   0,   0],
+ [ 50,  50,  50,  50,  50,  50,  50,  50],
+ [ 10,  10,  20,  30,  30,  20,  10,  10],
+ [  5,   5,  10,  25,  25,  10,   5,   5],
+ [  0,   0,   0,  20,  20,   0,   0,   0],
+ [  5,  -5, -10,   0,   0, -10,  -5,   5],
+ [  5,  10,  10, -20, -20,  10,  10,   5],
+ [  0,   0,   0,   0,   0,   0,   0,   0]]
+
+bparakeet = wparakeet[::-1]
+
+wnighthawk = [[-50, -40, -30, -30, -30, -30, -40, -50],
+ [-40, -20,   0,   0,   0,   0, -20, -40],
+ [-30,   0,  10,  15,  15,  10,   0, -30],
+ [-30,   5,  15,  20,  20,  15,   5, -30],
+ [-30,   0,  15,  20,  20,  15,   0, -30],
+ [-30,   5,  10,  15,  15,  10,   5, -30],
+ [-40, -20,   0,   5,   5,   0, -20, -40],
+ [-50, -40, -30, -30, -30, -30, -40, -50]]
+
+bnighthawk = wnighthawk[::-1]
+
+wbluejay = [[-20, -10, -10, -10, -10, -10, -10, -20],
+ [-10,   0,   0,   0,   0,   0,   0, -10],
+ [-10,   0,   5,  10,  10,   5,   0, -10],
+ [-10,   5,   5,  10,  10,   5,   5, -10],
+ [-10,   0,  10,  10,  10,  10,   0, -10],
+ [-10,  10,  10,  10,  10,  10,  10, -10],
+ [-10,   5,   0,   0,   0,   0,   5, -10],
+ [-20, -10, -10, -10, -10, -10, -10, -20]]
+
+bbluejay = wbluejay[::-1]
+
+wrobin = [[ 0,  0,  0,  0,  0,  0,  0,  0],
+ [ 5, 10, 10, 10, 10, 10, 10,  5],
+ [-5,  0,  0,  0,  0,  0,  0, -5],
+ [-5,  0,  0,  0,  0,  0,  0, -5],
+ [-5,  0,  0,  0,  0,  0,  0, -5],
+ [-5,  0,  0,  0,  0,  0,  0, -5],
+ [-5,  0,  0,  0,  0,  0,  0, -5],
+ [ 0,  0,  0,  5,  5,  0,  0,  0]]
+
+brobin = wrobin[::-1]
+
+wquetzal = [[-20, -10, -10,  -5,  -5, -10, -10, -20],
+ [-10,   0,   0,   0,   0,   0,   0, -10],
+ [-10,   0,   5,   5,   5,   5,   0, -10],
+ [ -5,   0,   5,   5,   5,   5,   0,  -5],
+ [  0,   0,   5,   5,   5,   5,   0,  -5],
+ [-10,   5,   5,   5,   5,   5,   0, -10],
+ [-10,   0,   5,   0,   0,   0,   0, -10],
+ [-20, -10, -10,  -5,  -5, -10, -10, -20]]
+
+bquetzal = wquetzal[::-1]
+
+wkingfisher = [[-30, -40, -40, -50, -50, -40, -40, -30],
+ [-30, -40, -40, -50, -50, -40, -40, -30],
+ [-30, -40, -40, -50, -50, -40, -40, -30],
+ [-30, -40, -40, -50, -50, -40, -40, -30],
+ [-20, -30, -30, -40, -40, -30, -30, -20],
+ [-10, -20, -20, -20, -20, -20, -20, -10],
+ [ 20,  20,   0,   0,   0,   0,  20,  20],
+ [ 20,  30,  10,   0,   0,  10,  30,  20]]
+
+bkingfisher = wkingfisher[::-1]
+
+
+def getVal(piece, row, col):
+    if piece == 'p':
+        return (bparakeet[row][col])
+    elif piece == 'P':
+        return (wparakeet[row][col])
+    elif piece == 'n':
+        return (bnighthawk[row][col])
+    elif piece == 'N':
+        return (wnighthawk[row][col])
+    elif piece == 'B':
+        return (wbluejay[row][col])
+    elif piece == 'b':
+        return (bbluejay[row][col])
+    elif piece == 'R':
+        return (wrobin[row][col])
+    elif piece == 'r':
+        return (brobin[row][col])
+    elif piece == 'Q':
+        return (wquetzal[row][col])
+    elif piece == 'q':
+        return (bquetzal[row][col])
+    elif piece == 'K':
+        return (wkingfisher[row][col])
+    elif piece == 'k':
+        return (bkingfisher[row][col])
+
+
+
+
+def getPoint(piece):
     return points[piece]
 
 def numEval(board, friend, foe):
@@ -31,9 +127,9 @@ def pieceEval(board, friend, foe):
     for x in range(0,board.shape[1]):
         for y in range(0,board.shape[0]):
             if board[x][y] in friend:
-                favor += getVal(board[x][y])
+                favor += getPoint(board[x][y])
             elif board[x][y] in foe:
-                against += getVal(board[x][y])
+                against += getPoint(board[x][y])
     return abs((favor - against))
 
 def rowEval(board, friend, foe):
@@ -47,6 +143,16 @@ def rowEval(board, friend, foe):
                 against += 7-x
     return abs((favor - against))
 
+def posEval(board, friend, foe):
+    favor = 0
+    against = 0
+    for x in range(0,board.shape[1]):
+        for y in range(0,board.shape[0]):
+            if board[x][y] in friend:
+                favor += getVal(board[x][y], x, y)
+            elif board[x][y] in foe:
+                against += getVal(board[x][y], x, y)
+    return abs((favor - against))
 
 def evalBoard(color,board):
     if color == 'w':
@@ -59,6 +165,7 @@ def evalBoard(color,board):
     rowWeight = rowEval(board, friend, foe)
     pieceWeight = pieceEval(board,friend,foe)
     numPieces = numEval(board,friend,foe)
+    positionalValue = posEval(board, friend, foe)
 
-    return rowWeight + pieceWeight + numPieces
+    return positionalValue + rowWeight + pieceWeight + numPieces
 
