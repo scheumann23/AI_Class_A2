fd2163dc5210ce50b500fd1d96af9ec5c1c79cce
vishal <vibhalla@iu.edu>
2020-10-27 23:54:57 -0700

    Changes to moveEval.py

--- part2/moveEval.py
+++ part2/moveEval.py
@@ -4,25 +4,27 @@ import re
 import numpy as np
 
 #
-# The points weightage has been taken from https://www.chessprogramming.org/Simplified_Evaluation_Function
+# The points and board weights have been taken 
+# from https://www.chessprogramming.org/Simplified_Evaluation_Function
 #
 #points = [100,320,330,500,900,20000]
-#p,n,b,r,q,k = points
-#P,N,B,R,Q,K = points
+
 points = {'p': 100,'n': 320,'b': 330,'r': 500,'q': 900,'k': 20000, 'P': 100,'N': 320,'B': 330,'R': 500,'Q': 900,'K': 20000}
+
+def getVal(piece):
+    return points[piece]
 
 
 def pieceEval(board, friend, foe):
-    favor = ''
-    against = ''
+    favor = 0
+    against = 0
     for x in range(0,board.shape[1]):
         for y in range(0,board.shape[0]):
             if board[x][y] in friend:
-                favor += ''.join(board[x][y])
+                favor += getVal(board[x][y])
             elif board[x][y] in foe:
-                against += ''.join(board[x][y])
-    #print(favor,against)
-    return 1
+                against += getVal(board[x][y])
+    return abs((favor - against))
 
 def rowEval(board, friend, foe):
     favor = 0
@@ -32,7 +34,7 @@ def rowEval(board, friend, foe):
             if board[x][y] in friend:
                 favor += x
             elif board[x][y] in foe:
-                against += 7 -x
+                against += 7-x
     return abs((favor - against))
 
 
@@ -45,6 +47,5 @@ def evalBoard(color,board):
         foe = 'PNBRQK'
     rowWeight = rowEval(board, friend, foe)
     pieceWeight = pieceEval(board,friend,foe)
-    #print(rowWeight,pieceWeight)
-    return rowWeight
+    return rowWeight + pieceWeight
 
